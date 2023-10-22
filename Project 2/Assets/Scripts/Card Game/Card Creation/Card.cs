67d7c007da55b2e4e83a7273686bbd3ae49d33dc
Dale-S <dalejsanchez2001@gmail.com>
2023-10-22 03:22:44 -0700

    Basic Card Creation Implemented

---
+++ Project 2/Assets/Scripts/Card Game/Card Creation/Card.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct Card
+{
+    public int health;
+    public int damage;
+}
