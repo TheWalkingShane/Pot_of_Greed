c43ed40ebbc387f0e95aba1b6da8fb16fa8fd2c9
Dale <78234740+Dale-S@users.noreply.github.com>
2023-11-07 20:33:22 -0800

    Camera Lerping

--- Project 2/Assets/Scripts/Card Game/Card Room/CameraSwitch.cs
+++ Project 2/Assets/Scripts/Card Game/Card Room/CameraSwitch.cs
@@ -7,26 +7,46 @@ public class CameraSwitch : MonoBehaviour
     public Transform cam1;
     public Transform cam2;
     private GameObject cam;
-    
+    private float smooth = 7;
+    private Vector3 newPosition;
+    private Quaternion newRotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
         cam = this.gameObject;
+        newPosition = transform.position;
+        newRotation = transform.rotation;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        changePosition();
+    }
+
+    private void changePosition()
+    {
+        Vector3 p1 = cam1.position;
+        Quaternion r1 = cam1.rotation;
+        Vector3 p2 = cam2.position;
+        Quaternion r2 = cam2.rotation;
+        
+        
         if (Input.GetKeyDown(KeyCode.W))
         {
-            cam.transform.position = cam2.position;
-            cam.transform.rotation = cam2.rotation;
+            newPosition = p2;
+            newRotation = r2;
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            cam.transform.position = cam1.position;
-            cam.transform.rotation = cam1.rotation;
+            newPosition = p1;
+            newRotation = r1;
         }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * smooth);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smooth);
     }
 }
